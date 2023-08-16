63dbd5737eae5a7260c4ec27cc5eb648436a1207
Luiz Gustavo <luizgustavoalves702@gmail.com>
2023-08-15 21:17:26 -0300

    build: created getHashtags middleware

--- src/routes/posts.routes.js
+++ src/routes/posts.routes.js
@@ -6,9 +6,14 @@ import {
     deletePost
 } from "../controllers/posts.controller.js";
 
+import { schemaValidation } from "../middlewares/schemaValidation.js";
+import { getHashtags } from "../middlewares/getHashtags.js";
+
+import { postsSchema } from "../schemas/posts.schema.js";
+
 const postsRouter = Router();
 
-postsRouter.post("/create-post", createPost);
+postsRouter.post("/create-post", schemaValidation(postsSchema), getHashtags, createPost);
 postsRouter.put("/update-post", updatePost);
 postsRouter.delete("/delete-post", deletePost);
 
