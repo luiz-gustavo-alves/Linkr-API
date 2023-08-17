d71a1aa7861f9999f3884b6d0bcea318de0f5a45
Geraldo Gomes <contato.gneto@gmail.com>
2023-08-17 20:28:51 -0300

    add local repository in cloud

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,15 +1,17 @@
 import { Router } from "express";
-
 import {
     signIn,
     signUp,
     logout
 } from "../controllers/auth.controller.js";
 
+import { schemaValidation } from "../middlewares/schemaValidation.js";
+import { userSignUpSchema, userSignInSchema } from "../schemas/auth.schema.js";
+
 const authRouter = Router();
 
-authRouter.post("/", signIn);
-authRouter.post("/sign-up", signUp);
+authRouter.post("/", schemaValidation(userSignInSchema), signIn);
+authRouter.post("/sign-up", schemaValidation(userSignUpSchema), signUp);
 authRouter.post("/logout", logout);
 
 export default authRouter;
--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,9 +1,45 @@
-import db from "../database/db.connection.js";
+import bcrypt from 'bcrypt'
+import db from '../database/db.connection.js'
 
+const register = async (payload) => {
+   const { name, email, imageURL, password } = payload
+   const encryptedPassword = bcrypt.hashSync(password, 10)
 
-
-const authService = {
-
+   await db.query(
+      `INSERT INTO users
+          (name, email, "imageURL", password)
+       VALUES ($1, $2, $3, $4);
+      `,
+      [name, email, imageURL, encryptedPassword]
+   )
 }
 
-export default authService;
+const login = async (payload) => {
+   const req = await findEmail(payload.email)
+   const password_encrypted = bcrypt.compareSync(payload.password, req.rows[0].password)
+
+   if (!req || !password_encrypted) return { success: false }
+
+   delete req.rows[0].password
+   return { success: true, data: req.rows[0] }
+}
+
+const findEmail = async (email) => {
+   const req = await db.query(
+      `SELECT *
+        FROM users
+        WHERE email = $1
+       `,
+      [email]
+   )
+
+   return req.rows[0] ? true : false
+}
+
+const authService = {
+   register,
+   login,
+   findEmail
+}
+
+export default authService
