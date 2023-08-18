8706456c65f8140c3ebf1e01097842a0713fb64c
Lucas-Benfica <lucassoaresbenfica@gmail.com>
2023-08-18 15:01:24 -0300

    feat: Posts página Usuário

--- src/services/users.service.js
+++ src/services/users.service.js
@@ -1,9 +1,14 @@
 import db from "../database/db.connection.js";
 
-
-
 async function userPosts(id){
-    const result = 0;
+    const result = await db.query(`
+    SELECT p."id", p."description", p."URL",
+    json_build_object('id', u."id", 'name', u."name", 'img', u."imageURL") AS "user"
+    FROM "posts" p
+    JOIN "users" u ON p."userID" = u."id"
+    WHERE u."id" = $1
+    ORDER BY p."createdAt" DESC;
+    `, [id]);
     return result;
 }
 
