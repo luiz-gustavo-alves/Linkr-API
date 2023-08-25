2ab6ba4cba39568abc717f562d31715b233a46eb
Luiz Gustavo <luizgustavoalves702@gmail.com>
2023-08-25 14:36:48 -0300

    fix: delete comments/likes/resposts from post

--- src/services/posts.service.js
+++ src/services/posts.service.js
@@ -56,9 +56,9 @@ const deletePost = async (postID, userID) => {
         hashtagsService.deleteHashtags(resultHashtagPost, postID);
     }
 
-    await db.query(`DELETE FROM resposts WHERE postID = $1;`, [postID]);
-    await db.query(`DELETE FROM likes WHERE postID = $1;`, [postID]);
-    await db.query(`DELETE FROM comments WHERE postID = $1;`, [postID]);
+    await db.query(`DELETE FROM resposts WHERE "postID" = $1;`, [postID]);
+    await db.query(`DELETE FROM likes WHERE "postID" = $1;`, [postID]);
+    await db.query(`DELETE FROM comments WHERE "postID" = $1;`, [postID]);
 
     await db.query(
         `DELETE FROM posts
